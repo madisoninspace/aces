88b87f4f394b7bceae01f6e6a7007cca99e2aefb
Madison L.H. Wass <931615+madisoninspace@users.noreply.github.com>
2024-04-15 00:52:56 -0400

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  aces
+//
+//  Created by Madison L.H. Wass on 4/15/24.
+//  Copyright __MyCompanyName__ 2024. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
